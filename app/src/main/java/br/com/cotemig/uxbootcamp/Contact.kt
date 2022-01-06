26606438768445276fd53a0b30b921bd47e038ae
marcioalv10 <marcioelx2@gmail.com>
2022-01-05 21:41:22 -0300

    Passagem de Dados entre Telas com Parcelable
    Adaptações na classe de contato para serializar os dados através das telas

--- app/src/main/java/br/com/cotemig/uxbootcamp/Contact.kt
+++ app/src/main/java/br/com/cotemig/uxbootcamp/Contact.kt
@@ -1,7 +1,11 @@
 package br.com.cotemig.uxbootcamp
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Contact(
         var name: String,
         var phone: String,
         var image: String,
-)
+) : Parcelable
--- app/src/main/java/br/com/cotemig/uxbootcamp/ContactDetail.kt
+++ app/src/main/java/br/com/cotemig/uxbootcamp/ContactDetail.kt
@@ -2,10 +2,32 @@ package br.com.cotemig.uxbootcamp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ContactDetail : AppCompatActivity() {
+
+    private var contact: Contact? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_detail)
+
+        getExtras()
+        bindViews()
+    }
+
+    private fun getExtras(){
+            contact = intent.getParcelableExtra(EXTRA_CONTACT)
+    }
+
+    //Método para setas os dados na tela
+    private fun bindViews(){
+        findViewById<TextView>(R.id.tv_nome).text = contact?.name
+        findViewById<TextView>(R.id.tv_telefone).text = contact?.phone
+
+    }
+
+    companion object{
+        const val EXTRA_CONTACT: String = "EXTRA_CONTACT"
     }
 }
